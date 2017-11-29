ce9435b82111672546ce0927928f546a2467edf9
Ryan Parlin <parlinr@ad.nmc.edu>
2017-11-29 14:25:39 -0500

    Begin repo work

---
+++ TermProject/TermProject/DAL/SQLRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProject
+{
+    public class SQLRepository
+    {
+        #region FIELDS
+        private List<Passers> _passers = new List<Passers>(); 
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        #endregion
+
+        #region METHODS
+
+        #endregion
+    }
+}
---
+++ TermProject/TermProject/Models/Passers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProject
+{
+    public class Passers
+    {
+        #region FIELDS
+        private double _score;
+        private string _lastName;
+        private string _firstName;
+        private string _teamNameLong;
+        private string _teamNameShort;
+        #endregion
+
+        #region PROPERTIES
+        public double Score { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string TeamNameLong { get; set; }
+        public string TeamNameShort { get; set; }
+
+
+        #endregion
+
+        #region CONSTRUCTORS
+        public Passers()
+        {
+
+        }
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+
+    }
+}
